d5beeaad6efdc4c4bd3f65ba9b6db7cbfe86cd59
jeseias <domingosjeseias@gmail.com>
2026-05-14 14:57:00 +0100

    feat: add _env

---
+++ api.patrol.sh/src/infra/config/_env.ts
@@ -0,0 +1,13 @@
+import z from "zod";
+
+const env_schema = z.object({
+  PORT: z.coerce.number().default(9696)
+}).strict()
+
+const { data, error } = env_schema.safeParse(Bun.env)
+
+if (error) {
+  throw new Error(`Invalid environment variables: ${error.message}`)
+}
+
+export const _env = data
